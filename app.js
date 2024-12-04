4a0b2cd1c3bdf76b8de5c05ab3026a5d1b176ad4
Aman Kumar <ak3230531@gmail.com>
2024-12-04 22:14:44 +0530

    Third party middleware

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
-
+const morgan = require('morgan')
 const app = express()
 
-
+app.use(morgan("dev"))
 app.set("view engine" , "ejs")
 
 
 // middleware
 
-app.use((req,res,next)=>{
+// app.use((req,res,next)=>{
+//   console.log("this is middleware")
+
+//   const a = 2 
+//   const b = 3
+//   console.log(a+b)
+
+//   return next()
+  
+// })
+
+app.get("/",(req,res,next)=>{
+
   console.log("this is middleware")
 
   const a = 2 
@@ -16,10 +28,7 @@ app.use((req,res,next)=>{
   console.log(a+b)
 
   return next()
-  
-})
-
-app.get("/",(req,res)=>{
+},(req,res)=>{
   // res.send("Home Page")
   res.render("index")
 })
