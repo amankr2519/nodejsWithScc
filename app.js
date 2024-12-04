13df3474e21d543ea0200b03238e3a5d6bf79c74
Aman Kumar <ak3230531@gmail.com>
2024-12-04 18:56:39 +0530

    middleware

--- app.js
+++ app.js
@@ -2,8 +2,26 @@ const express = require('express')
 
 const app = express()
 
+
+app.set("view engine" , "ejs")
+
+
+// middleware
+
+app.use((req,res,next)=>{
+  console.log("this is middleware")
+
+  const a = 2 
+  const b = 3
+  console.log(a+b)
+
+  return next()
+  
+})
+
 app.get("/",(req,res)=>{
-  res.send("Home Page")
+  // res.send("Home Page")
+  res.render("index")
 })
 
 app.get("/about",(req,res)=>{
